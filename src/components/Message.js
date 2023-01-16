b2a2f56efdc921a3c93c90484ded40ccd72c1e92
Hadeel Abdelkareem <43421729+hadeelgamal@users.noreply.github.com>
2023-01-16 15:28:34 +0200

    done

---
+++ src/components/Message.js
@@ -0,0 +1,11 @@
+function Message(props){
+    return (
+        <div className="message"> 
+        <p className="message">
+          {props.message}
+        </p>
+        </div>
+    )
+}
+
+export default Message;
